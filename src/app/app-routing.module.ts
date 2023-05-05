6049d3138544fae4b2c6b459ab810dbccefae740
SHUBHAM VERMA <shubhamverma9210@gmail.com>
2023-05-06 01:57:38 +0530

    v1 work flow complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { SignupGuardService } from '@core/guards/singup.guard.service';
+ 
+  const loadLoginModule = () =>
+  import('./login/login.module').then(m => m.LoginModule);
+  const loadElderlyModule = () =>
+  import('./elderly/elderly.module').then(m => m.ElderlyModule);
+  const loadVolunteerModule = () =>
+  import('./volunteer/volunteer.module').then(m => m.VolunteerModule);
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:loadLoginModule,
+  },
+  {
+    path:'elderly',
+    canActivate:[SignupGuardService],
+    loadChildren:loadElderlyModule,
+  },
+  {
+    path:'volunteer',
+    canActivate:[SignupGuardService],
+    loadChildren:loadVolunteerModule,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
